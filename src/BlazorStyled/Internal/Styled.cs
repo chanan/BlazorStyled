f056b1f9af4c3a8986a6d5b4f6ba7079bfd5a83e
Chanan Braunstein <chanan@bkbunch.com>
2019-07-16 21:37:19 -0600

    #10

--- src/BlazorStyled/Internal/Styled.cs
+++ src/BlazorStyled/Internal/Styled.cs
@@ -31,7 +31,14 @@ namespace BlazorStyled.Internal
                 else
                 {
                     rule = ParsePredefinedRuleSet(className, css);
-                    await AddUniqueRuleSetToStyleSheet(rule);
+                    if(_elements.Contains(className))
+                    {
+                        await AddNonUniqueRuleSetToStyleSheet(rule);
+                    }
+                    else
+                    {
+                        await AddUniqueRuleSetToStyleSheet(rule);
+                    }
                 }
                 return rule.Selector;
             }
@@ -286,5 +293,125 @@ namespace BlazorStyled.Internal
                 await _styledJsInterop.InsertRule(rule.ToString());
             }
         }
+
+        private async Task AddNonUniqueRuleSetToStyleSheet(IRule rule)
+        {
+            _styleSheet.Classes.Add(rule);
+            await _styledJsInterop.InsertRule(rule.ToString());
+        }
+
+        private List<string> _elements = new List<string>
+        {
+            "a",
+            "abbr",
+            "address",
+            "area",
+            "article",
+            "aside",
+            "audio",
+            "b",
+            "base",
+            "bdi",
+            "bdo",
+            "blockquote",
+            "body",
+            "br",
+            "button",
+            "canvas",
+            "caption",
+            "cite",
+            "code",
+            "col",
+            "colgroup",
+            "data",
+            "datalist",
+            "dd",
+            "del",
+            "details",
+            "dfn",
+            "dialog",
+            "div",
+            "dl",
+            "dt",
+            "em",
+            "embed",
+            "fieldset",
+            "figcaption",
+            "figure",
+            "footer",
+            "form",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "head",
+            "header",
+            "hr",
+            "html",
+            "i",
+            "iframe",
+            "img",
+            "input",
+            "ins",
+            "kbd",
+            "label",
+            "legend",
+            "li",
+            "link",
+            "main",
+            "map",
+            "mark",
+            "meta",
+            "meter",
+            "nav",
+            "noscript",
+            "object",
+            "ol",
+            "optgroup",
+            "option",
+            "output",
+            "p",
+            "param",
+            "picture",
+            "pre",
+            "progress",
+            "q",
+            "rp",
+            "rt",
+            "ruby",
+            "s",
+            "samp",
+            "script",
+            "section",
+            "select",
+            "small",
+            "source",
+            "span",
+            "strong",
+            "style",
+            "sub",
+            "summary",
+            "sup",
+            "svg",
+            "table",
+            "tbody",
+            "td",
+            "template",
+            "textarea",
+            "tfoot",
+            "th",
+            "thead",
+            "time",
+            "title",
+            "tr",
+            "track",
+            "u",
+            "ul",
+            "var",
+            "video",
+            "wbr"
+        };
     }
 }
